80319686445092b02c0d9eb3b3446a9cce4858e5
jihad <allaouijihad05@gmail.com>
2024-12-25 16:34:25 +0100

    first commit

---
+++ src/main/java/com/jihad/edunest/EduNestApplication.java
@@ -0,0 +1,13 @@
+package com.jihad.edunest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduNestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduNestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jihad/edunest/EduNestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jihad.edunest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduNestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
